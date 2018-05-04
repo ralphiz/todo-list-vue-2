f4f5b6d36942aa6a94997aeec78804bd58889c35
Ralph Cacho <cacho.rc@gmail.com>
2018-05-03 19:55:09 -0700

    Update App to use TodoList component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div>
+    <TodoList v-bind:todos="todos"/>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import TodoList from './components/TodoList';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    TodoList,
+  },
+  data() {
+    return {
+      todos: [{
+        id: 1,
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        id: 2,
+        title: 'Todo B',
+        project: 'Project B',
+        done: false,
+      }, {
+        id: 3,
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        id: 4,
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
   },
 };
 </script>
